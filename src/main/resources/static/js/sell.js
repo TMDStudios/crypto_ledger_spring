ad886e606074d4a74f7f7cbaf24d64ae7859a846
TMDStudios <tmdstudioscontact@gmail.com>
2022-04-25 21:58:12 -0400

    User can no longer sell more than they have

--- src/main/resources/static/js/sell.js
+++ src/main/resources/static/js/sell.js
@@ -1,18 +1,24 @@
-function sell(coinId){
-	let amount = prompt("Amount: ");
+function sell(coinId, coinTotal){
+	let amount = prompt("Enter Amount:");
 	if (amount > 0) {
-		let xhttp = new XMLHttpRequest();
-	  	xhttp.open("POST", "/api/sell");
-		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xhttp.send("id="+coinId+"&amount="+amount);
-		
-		if (confirm("Sold "+amount)) {
-			console.log("AMT:"+amount+" id: "+coinId);
-		  	window.open("/home", "_self");
-		} else {
-		  	console.log("Clicked Cancel");
+			
+		if (amount <= coinTotal){
+			let xhttp = new XMLHttpRequest();
+		  	xhttp.open("POST", "/api/sell");
+			xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			xhttp.send("id="+coinId+"&amount="+amount);
+			if (confirm("Sold "+amount)) {
+				console.log("AMT:"+amount+" id: "+coinId);
+			  	window.open("/home", "_self");
+			} else {
+			  	console.log("Clicked Cancel");
+			}
+		}else {
+			alert("The sale amount cannot be larger than your coin total");
 		}
-	} else {
-	  	prompt("Amount must be greater than 0");
+		
+		
+	}else {
+	  	alert("Amount must be greater than 0");
 	}
 }
--- src/main/webapp/WEB-INF/home.jsp
+++ src/main/webapp/WEB-INF/home.jsp
@@ -71,7 +71,7 @@
 				<td class="red"><img src="${downArrow}"/> <fmt:formatNumber pattern="0.000" value="${coin.priceDifference}"/>%</td>
 			</c:if>
 			<td scope="col" class="center-cell">
-			<button type="button" onclick="sell(document.getElementById('${coin.id}').value)" id="${coin.id}" value="${coin.id}">Sell</button>
+			<button type="button" onclick="sell(document.getElementById('${coin.id}').value, ${coin.totalAmount})" id="${coin.id}" value="${coin.id}">Sell</button>
 			</td>
 		</c:if>
 		</tr>
