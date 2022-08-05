2009bb5124b18eb9bb75b4d6978b68430656c703
TMDStudios <tmdstudioscontact@gmail.com>
2022-08-04 22:58:53 -0400

    API is automatically generated when user registers

--- src/main/java/com/tmdstudios/cls/services/UserService.java
+++ src/main/java/com/tmdstudios/cls/services/UserService.java
@@ -24,13 +24,12 @@ public class UserService {
 	UserRepo userRepo;
 	
 	public User register(User newUser, BindingResult result) {
-		// Reject if username is taken (present in database)
     	Optional<User> potentialUser = userRepo.findByUsername(newUser.getUsername());
 
     	if(potentialUser.isPresent()) {
     		result.rejectValue("username", "UsernameTaken", "An account with that username already exists!");
     	}
-        // Reject if password doesn't match confirmation
+
     	if(!newUser.getPassword().equals(newUser.getConfirm())) {
     		result.rejectValue("confirm", "Matches", "The Confirm Password must match Password!");
     	}
@@ -39,30 +38,27 @@ public class UserService {
 	        return null;
 	    }
     
-        // Hash and set password, save user to database
     	String hashed = BCrypt.hashpw(newUser.getPassword(), BCrypt.gensalt());
     	newUser.setPassword(hashed);
+    	
+    	newUser.setApiKey(generateApiKey(newUser));
+    	
     	return userRepo.save(newUser);
 	}
 	
 	public User login(LoginUser newLogin, BindingResult result) {
-    	// TO-DO - Reject values:
     	Optional<User> potentialUser = userRepo.findByUsername(newLogin.getUsername());
         
-    	// Find user in the DB by username
-        // Reject if NOT present
     	if(!potentialUser.isPresent()) {
     		result.rejectValue("username", "Matches", "User not found!");
     		return null;
     	}
-        
-        // Reject if BCrypt password match fails
+
     	if(!BCrypt.checkpw(newLogin.getPassword(), potentialUser.get().getPassword())) {
     	    result.rejectValue("password", "Matches", "Invalid Password!");
     	    return null;
     	}
-    	
-        // Otherwise, return the user object
+
         return potentialUser.get();
     }
 	
