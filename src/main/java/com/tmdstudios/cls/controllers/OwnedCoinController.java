9f8a000e58ca9bcdc5bb42eb463be0f7ac74b8de
TMDStudios <tmdstudioscontact@gmail.com>
2022-08-12 23:28:21 -0400

    Fixed price difference for owned coins

--- src/main/java/com/tmdstudios/cls/controllers/OwnedCoinController.java
+++ src/main/java/com/tmdstudios/cls/controllers/OwnedCoinController.java
@@ -81,7 +81,8 @@ public class OwnedCoinController {
 		newOwnedCoin.setTotalSpent(totalSpent);
 		newOwnedCoin.setPurchasePrice(totalSpent/totalAmount);
 		newOwnedCoin.setCurrentPrice(currentPrice);
-		newOwnedCoin.setPriceDifference(currentPrice-purchasePrice*100-100);
+//		newOwnedCoin.setPriceDifference(currentPrice-purchasePrice*100-100);
+		newOwnedCoin.setPriceDifference((coin.getPrice()/(totalSpent/totalAmount))*100-100);
 		newOwnedCoin.setTotalProfit(currentPrice*totalAmount-totalSpent/totalAmount*totalAmount);
 		ownedCoinService.addOwnedCoin(newOwnedCoin);
 		
